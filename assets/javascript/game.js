42e7510ffad463609596c172ec5ff65ee65c213b
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-05-06 12:14:55 -0600

    Styling worked on

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,21 @@
+var win = 0;
+var lose = 0;
+var allGems
+var red
+var blue
+var green
+var yellow
+var winHtml
+var lossHtml
+
+
+
+
+
+
+
+
+
 function Random (min, max){
     return;
     Math.floor(math.random()*max) + min;
