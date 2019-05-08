e7acc0c9eb4e7f4eb63711a2b79e050e6afd7d5c
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-05-08 12:50:27 -0600

    Stuff mostly works, need to reset game and style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,94 @@
 var win = 0;
 var lose = 0;
-var allGems
-var red
-var blue
-var green
-var yellow
-var winHtml
-var lossHtml
+var target = [Math.floor(Math.random() * (120 - 19) + 19)];
+var allGems = [Math.floor(Math.random() * (12 - 1) + 1)];
+// var red = [Math.floor(Math.random() * (12-1)+1)];
+// var orange = [Math.floor(Math.random() * (12-1)+1)];
+// var blue = [Math.floor(Math.random() * (12-1)+1)];
+// var green = [Math.floor(Math.random() * (12-1)+1)];
+var winHtml = 0
+var lossHtml = "";
+var yourScore = 0;
+$("#compNum").text(target);
 
 
 
 
 
 
-
-
-
-function Random (min, max){
-    return;
-    Math.floor(math.random()*max) + min;
+function Random(min, max) {
+    return Math.floor(Math.random() * max) + min;
 }
 
-Random (19, 120)
+var Colors = [{
+        name: "Red",
+        image: "assets/images/star.png",
+        value: Random(1, 12)
+    },
+    {
+        name: "Orange",
+        image: "assets/images/ruby.png",
+        value: Random(1, 12)
+    },
+    {
+        name: "Blue",
+        image: "assets/images/star.png",
+        value: Random(1, 12)
+    },
+    {
+        name: "Green",
+        image: "assets/images/ruby.png",
+        value: Random(1, 12)
+    }
+
+]
+
+
+
+console.log(target);
+console.log(allGems);
+
+
+
+for (var i = 0; i < Colors.length; i++) {
+    // allGems = [Math.floor(Math.random() * (12-1)+1)];
+    var gems = Random(Colors);
+    // Colors[i].innerHTML = Colors.name + '<img src="'+Colors.image+'">';
+    var imageCrystal = $("<img>");
+
+    imageCrystal.addClass("crystal-image");
+
+    imageCrystal.attr("src", Colors[i].image);
+
+    imageCrystal.attr("data", Colors[i].value);
+
+    $("#crystals").append(imageCrystal);
+}
+
+
+$(".crystal-image").on("click", function () {
+
+    var crystalValue = ($(this).attr("data"));
+    crystalValue = parseInt(crystalValue);
+    yourScore += crystalValue;
+    $("#yourScore").text(yourScore);
+
+
+    if (yourScore == target) {
+        win += 1;
+        yourScore = 0;
+        $("#wins").text(win);
+        Random(1, 12);
+        target = [Math.floor(Math.random() * (120 - 19) + 19)];
+
+        console.log(win + "YOU WIN!");
+    } 
+    else if (yourScore > target) {
+        lose += 1;
+        yourScore = 0;
+        $("#losses").text(lose);
+        Random(1, 12);
+        target = [Math.floor(Math.random() * (120 - 19) + 19)];
+    }
+
+})
