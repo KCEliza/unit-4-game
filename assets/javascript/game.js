ad8f4b8505e9d92ef159aad7e86101fa8aff2210
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-05-09 17:43:39 -0600

    Functions, needs to style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,11 @@
 var win = 0;
 var lose = 0;
-var target = [Math.floor(Math.random() * (120 - 19) + 19)];
 var allGems = [Math.floor(Math.random() * (12 - 1) + 1)];
 // var red = [Math.floor(Math.random() * (12-1)+1)];
 // var orange = [Math.floor(Math.random() * (12-1)+1)];
 // var blue = [Math.floor(Math.random() * (12-1)+1)];
 // var green = [Math.floor(Math.random() * (12-1)+1)];
-var winHtml = 0
-var lossHtml = "";
 var yourScore = 0;
-$("#compNum").text(target);
 
 
 
@@ -20,47 +16,31 @@ function Random(min, max) {
     return Math.floor(Math.random() * max) + min;
 }
 
-var Colors = [{
-        name: "Red",
-        image: "assets/images/star.png",
-        value: Random(1, 12)
-    },
-    {
-        name: "Orange",
-        image: "assets/images/ruby.png",
-        value: Random(1, 12)
-    },
-    {
-        name: "Blue",
-        image: "assets/images/star.png",
-        value: Random(1, 12)
-    },
-    {
-        name: "Green",
-        image: "assets/images/ruby.png",
-        value: Random(1, 12)
-    }
 
-]
+var Colors = ["assets/images/star.png", "assets/images/ruby.png", "assets/images/star.png", "assets/images/ruby.png"];
+        
 
 
 
-console.log(target);
-console.log(allGems);
 
 
+
+loop();
+function loop (){
+    var target = [Math.floor(Math.random() * (120 - 19) + 19)];
+    $("#compNum").text(target);
 
 for (var i = 0; i < Colors.length; i++) {
     // allGems = [Math.floor(Math.random() * (12-1)+1)];
     var gems = Random(Colors);
-    // Colors[i].innerHTML = Colors.name + '<img src="'+Colors.image+'">';
+    
     var imageCrystal = $("<img>");
 
     imageCrystal.addClass("crystal-image");
 
-    imageCrystal.attr("src", Colors[i].image);
-
-    imageCrystal.attr("data", Colors[i].value);
+    imageCrystal.attr("src", Colors[i]);
+    
+    imageCrystal.attr("data", Random(1, 12));
 
     $("#crystals").append(imageCrystal);
 }
@@ -76,19 +56,26 @@ $(".crystal-image").on("click", function () {
 
     if (yourScore == target) {
         win += 1;
-        yourScore = 0;
-        $("#wins").text(win);
-        Random(1, 12);
-        target = [Math.floor(Math.random() * (120 - 19) + 19)];
 
-        console.log(win + "YOU WIN!");
+        $("#wins").text(win);
+        $("#crystals").append(imageCrystal);
+        reset();
     } 
     else if (yourScore > target) {
         lose += 1;
-        yourScore = 0;
         $("#losses").text(lose);
-        Random(1, 12);
-        target = [Math.floor(Math.random() * (120 - 19) + 19)];
+        reset();
+        
     }
 
 })
+function reset(){
+    
+    $("#compNum, #yourScore, #crystals").empty();
+    yourScore = 0;
+    $("#yourScore").text(yourScore);
+    loop();
+    
+
+}
+}
