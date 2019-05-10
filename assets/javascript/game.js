695e1ad2015d3d787bb6c7d54318b33c7eb81cec
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-05-09 22:16:05 -0600

    done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,5 @@
 var win = 0;
 var lose = 0;
-var allGems = [Math.floor(Math.random() * (12 - 1) + 1)];
-// var red = [Math.floor(Math.random() * (12-1)+1)];
-// var orange = [Math.floor(Math.random() * (12-1)+1)];
-// var blue = [Math.floor(Math.random() * (12-1)+1)];
-// var green = [Math.floor(Math.random() * (12-1)+1)];
 var yourScore = 0;
 
 
@@ -17,7 +12,7 @@ function Random(min, max) {
 }
 
 
-var Colors = ["assets/images/star.png", "assets/images/ruby.png", "assets/images/star.png", "assets/images/ruby.png"];
+var Colors = ["assets/images/Frog.jpg", "assets/images/Snake.jpg", "assets/images/Turtle.jpg", "assets/images/Snail.jpg"];
         
 
 
@@ -34,23 +29,23 @@ for (var i = 0; i < Colors.length; i++) {
     // allGems = [Math.floor(Math.random() * (12-1)+1)];
     var gems = Random(Colors);
     
-    var imageCrystal = $("<img>");
+    var animalsImage = $("<img>");
 
-    imageCrystal.addClass("crystal-image");
+    animalsImage.addClass("animal-image");
 
-    imageCrystal.attr("src", Colors[i]);
+    animalsImage.attr("src", Colors[i]);
     
-    imageCrystal.attr("data", Random(1, 12));
+    animalsImage.attr("data", Random(1, 12));
 
-    $("#crystals").append(imageCrystal);
+    $("#animals").append(animalsImage);
 }
 
 
-$(".crystal-image").on("click", function () {
+$(".animal-image").on("click", function () {
 
-    var crystalValue = ($(this).attr("data"));
-    crystalValue = parseInt(crystalValue);
-    yourScore += crystalValue;
+    var animalValue = ($(this).attr("data"));
+    animalValue = parseInt(animalValue);
+    yourScore += animalValue;
     $("#yourScore").text(yourScore);
 
 
@@ -58,7 +53,7 @@ $(".crystal-image").on("click", function () {
         win += 1;
 
         $("#wins").text(win);
-        $("#crystals").append(imageCrystal);
+        $("#animals").append(animalsImage);
         reset();
     } 
     else if (yourScore > target) {
@@ -71,7 +66,7 @@ $(".crystal-image").on("click", function () {
 })
 function reset(){
     
-    $("#compNum, #yourScore, #crystals").empty();
+    $("#compNum, #yourScore, #animals").empty();
     yourScore = 0;
     $("#yourScore").text(yourScore);
     loop();
